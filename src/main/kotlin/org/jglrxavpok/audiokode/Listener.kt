bb463ac535ae166e87b971f1635aba5e51cfccdd
jglrxavpok <jglrxavpok@gmail.com>
2017-08-29 13:04:18 +0200

    Changed default up position

--- src/main/kotlin/org/jglrxavpok/audiokode/Listener.kt
+++ src/main/kotlin/org/jglrxavpok/audiokode/Listener.kt
@@ -4,6 +4,6 @@ class Listener {
 
     var position: Vector3D = NullVector
     var velocity: Vector3D = NullVector
-    var up: Vector3D = NullVector
+    var up: Vector3D = Vector3D(0f, 1f, 0f)
     var lookDirection: Vector3D = NullVector
 }
